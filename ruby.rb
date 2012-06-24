3c04ab2a60374af944e0f15f2e385f0983b45ca7
Lowell Kirsh <lowellk@gmail.com>
2012-06-24 14:32:14 -0700

    add ruby script

---
+++ ruby.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+
+require 'rubygems' # needed for Ruby 1.8
+require 'pp' # pretty-print
+require 'active_support'
+
+def main
+  puts 'hello world'
+end
+
+if __FILE__ == $PROGRAM_NAME
+  main
+end
